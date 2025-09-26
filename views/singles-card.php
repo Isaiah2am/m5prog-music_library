1590644925999d552fdfd4e3ee6a679b801fdf80
Isaiah2am <isaiahkoroma@hotmail.com>
2025-09-26 14:50:25 +0200

    opdracht 4

--- views/singles-card.php
+++ views/singles-card.php
@@ -1,11 +1,4 @@
-<div style="
-        border: 1px solid #ccc;
-        border-radius: 8px;
-        padding: 20px;
-        max-width: 300px;
-        box-shadow: 2px 2px 10px rgba(0,0,0,0.1);
-        margin-top: 20px;
-    ">
+<div class="card">
         <h2>Card Title</h2>
         <p>This is a simple div card with some content inside.</p>
     </div>
